6f43c931f2b2c5c79684b307ad3c585f05dfe4a5
Eric Ferguson <ferguson.eric.r@gmail.com>
2021-04-14 14:16:33 -0700

    js > added = & backhour, tested, works

--- JS/calculator.js
+++ JS/calculator.js
@@ -1,12 +1,11 @@
-
 let runningTotal = 0;
 let buffer = "0";
-let previousOperator;
+let previousOperator = null;
 
 const screen = document.querySelector('.screen');
 
 function buttonClick(value){
-    if(isNaN(value)){
+    if ( isNaN(value) ){
         // this is not a number
         handleSymbol(value);
     } else {
@@ -16,52 +15,98 @@ function buttonClick(value){
     screen.innerText = buffer; 
 };
 
-function handleSymbol(symbol){
-    // if(symbol === "C"){
-    //     buffer = "0";
-    //     runningTotal = 0;
-    // }
 
-    switch(value){
-        case "C":
-            buffer = "0";
-            runningTotal = 0;
-            break;
-        case "&plus":
-        case "&minus":
-        case "&times":
-        case "&divide":
-            handelMath(symbol);
-            break;
+
+function handleNumber(value){
+    if( buffer === "0" ){
+        buffer = value;
+    } else {
+        buffer += value
+        // buffer = buffer + value
     }
 }
 
-function handleMath(symbol){
+function handleMath(value){
+    console.log('handle Math', value)
+
     if ( buffer === "0"){
         // do nothing
         return
     }
     const intBuffer = parseInt(buffer);
     // const intBuffer = +buffer; -- same code/outcome as above
-
     if ( runningTotal === 0){
          runningTotal = intBuffer;
     } else {
         flushOperation(intBuffer);
-
     }
-    previousOperator = symbol;
-
+    previousOperator = value;
     buffer = "0";
 }
 
-function handleNumber(numberString){
-    if(buffer === "0"){
-        buffer = numberString;
+function flushOperation(intBuffer){
+    // switch( previousOperator){
+    //     case "+":
+    //         runningTotal += intBuffer;
+    //         break;
+    //     case "-":
+    //         runningTotal -= intBuffer;
+    //         break;
+    //     case "x":
+    //         runningTotal *= intBuffer;
+    //         break;
+    //     case "÷":
+    //         runningTotal /= intBuffer;
+    // }
+    if( previousOperator === "+"){
+        runningTotal += intBuffer;
+    } else if ( previousOperator === "−"){
+        runningTotal -= intBuffer;
+    } else if ( previousOperator === "×"){
+        runningTotal *= intBuffer;
     } else {
-        buffer = buffer + numberString
+        runningTotal /= intBuffer;
     }
-    console.log('buffer', buffer)
+    console.log('running total', runningTotal)
+}
+
+function handleSymbol(symbol){
+    console.log('handle symbol', symbol)
+    // if(symbol === "C"){
+    //     buffer = "0";
+    //     runningTotal = 0;
+    // }
+
+    switch(symbol){
+        case "C":
+            buffer = "0";
+            runningTotal = 0;
+            break;
+        case '=':
+            if ( previousOperator === null ){
+                // need two numbers to do math
+                // edge case?
+                return 
+            }
+            flushOperation( parseInt(buffer) );
+            previousOperator = null;
+            buffer = runningTotal;
+            runningTotal = 0;
+            break;
+        case "←":
+            if ( buffer.length === 1){
+                buffer = "0";
+            } else {
+                buffer = buffer.substring(0, buffer.length - 1)
+            }
+            break;
+        case "+":
+        case "−":
+        case "×":
+        case "÷":
+            handleMath(symbol);
+            break;
+    }
 }
 
 function init(){
