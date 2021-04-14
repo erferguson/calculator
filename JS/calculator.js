68f2a06f9a854dcd643771b51f167184ae7ce669
Eric Ferguson <ferguson.eric.r@gmail.com>
2021-04-14 10:44:06 -0700

    js > buttonClick, handleSymbol, handleNumber functions created & scouped out

---
+++ JS/calculator.js
@@ -0,0 +1,40 @@
+
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+
+const screen = document.querySelector('.screen');
+
+function buttonClick(value){
+    if(isNaN(value)){
+        // this is not a number
+        handleSymbol(value);
+    } else {
+        // this is a number
+        handleNumber(value); 
+    }
+        
+};
+
+function handleSymbol(symbol){}
+
+function handleNumber(numberString){
+    if(buffer === "0"){
+        buffer = numberString;
+    } else {
+        buffer = buffer + numberString
+    }
+}
+
+
+
+
+function init(){
+    document.querySelector('.calc-buttons')
+    .addEventListener('click', function(event){
+        buttonClick(event.target.innerText)
+    })
+}
+init();
+
+
